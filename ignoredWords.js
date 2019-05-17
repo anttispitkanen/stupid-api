ff8d16e9704bef59798372ba363fa06d851cf760
Antti Pitkänen <anttis.pitkanen@gmail.com>
2019-05-17 23:28:06 +0300

    I bet some progress has taken place

---
+++ ignoredWords.js
@@ -0,0 +1,21 @@
+const ignoredWords = [
+  'a',
+  'an',
+  'is',
+  'was',
+  'the',
+  'in',
+  'at',
+  'for',
+  'of',
+  'and',
+  'who',
+  'from',
+  'to',
+  'he',
+  'her',
+  'he',
+  'his'
+];
+
+module.exports = { ignoredWords };
--- index.js
+++ index.js
@@ -1,40 +1,98 @@
-const cheerio = require('cheerio');
 const axios = require('axios');
+const { ignoredWords } = require('./ignoredWords');
+
+const { RAPIDAPI_KEY, RAPIDAPI_HOST } = process.env;
 
 (async () => {
-  const queryTerm = 'football';
+  const lang = 'simple';
+  const queryTerm = 'taylor swift';
 
+  const { description, link } = await fetchSentenceFromWikipedia(
+    lang,
+    queryTerm
+  );
+
+  console.log(description);
+  console.log();
+  console.log(link);
+  console.log();
+
+  const words = extractWords(description, ignoredWords);
+  console.log(words); // FIXME:
+  console.log();
+  console.log(replaceWords(description, words));
+
+  console.log(await createSynonyms(words));
+})();
+
+async function fetchSentenceFromWikipedia(lang, queryTerm) {
   const response = await axios.get(
-    `https://simple.wikipedia.org/w/api.php?action=opensearch&redirects=resolve&search=${encodeURIComponent(
+    `https://${lang}.wikipedia.org/w/api.php?action=opensearch&redirects=resolve&search=${encodeURIComponent(
       queryTerm
     )}`
   );
   const { data } = response;
+  const description = data && data[2] && data[2][0];
+  const link = data && data[3] && data[3][0];
+  return { description, link };
+}
 
-  const description = data[2][0];
-  const link = data[3][0];
-  console.log(description);
-  console.log(link);
-  console.log();
+function extractWords(text, ignoredWordArray) {
+  return text
+    .split(/\W/)
+    .filter(
+      word =>
+        !!word &&
+        /\D/.test(word) &&
+        !ignoredWordArray.includes(word.toLowerCase())
+    );
+}
 
-  const htmlResponse = await axios.get(link);
-  const html = htmlResponse.data;
-  // console.log(html);
-  const $ = cheerio.load(html);
-  const paragraphs = [];
-  const cheerioPath =
-    $('.mw-parser-output').length === 0
-      ? '#mw-content-text > p'
-      : '#mw-content-text > .mw-parser-output > p';
-
-  $(cheerioPath).each((i, elem) =>
-    paragraphs.push(
-      $(elem)
-        .text()
-        .replace(/\[\d+\]/g, '')
-    )
+function replaceWords(text, wordArray) {
+  let textToReturn = text;
+  wordArray.forEach(
+    word => (textToReturn = textToReturn.replace(word, 'KISSE'))
   );
+  return textToReturn;
+}
 
-  // console.log(paragraphs);
-  paragraphs.forEach(p => console.log(p));
-})();
+/**
+ * Fetch synonyms for each word given
+ * @param {Array<string>} wordArray
+ */
+async function createSynonyms(wordArray) {
+  const wordsWithSynonyms = [];
+  await Promise.all(
+    wordArray.map(async word => {
+      const wordWithSynonyms = await fetchSynonyms(word);
+      return wordsWithSynonyms.push(wordWithSynonyms);
+    })
+  );
+  return wordsWithSynonyms;
+}
+
+async function fetchSynonyms(word) {
+  const response = await axios.get(
+    `https://wordsapiv1.p.rapidapi.com/words/${encodeURIComponent(
+      word
+    )}/synonyms`,
+    {
+      headers: {
+        'x-RapidAPI-Host': RAPIDAPI_HOST,
+        'x-RapidAPI-Key': RAPIDAPI_KEY
+      }
+    }
+  );
+  const { data } = response;
+  return data;
+}
+
+function takeLongestWord(words) {
+  let word = '';
+  words.forEach(w => {
+    if (w.length > word.length) {
+      word = w;
+    }
+  });
+  return word;
+}
